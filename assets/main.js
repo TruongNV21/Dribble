6a80f4195c888251dd73f6280bc8b88a42be1335
Nguyen Van Truong <ttntruong79@gamil.com>
2023-03-02 01:09:26 +0700

    update2productwraper

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,14 @@
 const mobileButtonMenu = document.querySelector(".navbar__mobile-menu-toggle.mobile-only .button--menu");
 const mobileCloseMenu = document.querySelector(".navbar__mobile-menu-toggle.mobile-only .button--close")
 const mobileHeaderMenu = document.querySelector(".header-mobile-menu");
+const filterViewButton = document.querySelector(".filter-view");
+const filterSettingButon = document.querySelector(".filter-settings");
+const producFilterShot = document.querySelector(".product__filter-shot");
+const choiceDateBox = document.querySelector(".find-shot__input-dropdown-click.date");
+const dateOptions = document.querySelector(".find-shot__input-dropdown-click.date .input-dropdown-click__options");
+const choiceDownload = document.querySelector('.find-shot__input-dropdown-click.downloads')
 
-console.log(mobileButtonMenu,mobileHeaderMenu)
+
 mobileButtonMenu.addEventListener('click',()=>{
     mobileHeaderMenu.classList.remove('hidden');
     mobileButtonMenu.classList.add('hidden');
@@ -13,4 +19,20 @@ mobileCloseMenu.addEventListener('click',()=>{
     mobileHeaderMenu.classList.add('hidden');
     mobileButtonMenu.classList.remove('hidden');
     mobileCloseMenu.classList.add('hidden');
+})
+
+filterViewButton.addEventListener('click',()=>{
+    filterViewButton.classList.toggle('close')
+})
+
+filterSettingButon.addEventListener('click',()=>{
+    producFilterShot.classList.toggle('hidden')
+})
+
+choiceDateBox.addEventListener('click', ()=>{
+    choiceDateBox.classList.toggle('hidden')
+})
+
+choiceDownload.addEventListener('click', ()=>{
+    choiceDownload.classList.toggle('hidden')
 })
